29477e6e8ecc8080007e0842d6713fbdcfacecf1
Bahadır Ahmet Ulaş <bahadirahmetu@gmail.com>
2024-03-04 18:02:25 +0300

    merhaba

--- src/main/java/Sudoku/GameBoard.java
+++ src/main/java/Sudoku/GameBoard.java
@@ -4,6 +4,7 @@ public class GameBoard
 {
     int[][] gameBoard;
 
+    // merhaba
     public GameBoard(int dimensionNumber)
     {
         this.gameBoard = new int[dimensionNumber][dimensionNumber];
