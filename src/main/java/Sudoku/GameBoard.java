f49ef10fc41c66ef4b9fc5f1421400b01bdd0662
Bahadir34 <bahadirahmetu@gmail.com>
2024-03-04 18:06:21 +0300

    hdhdf

--- src/main/java/Sudoku/GameBoard.java
+++ src/main/java/Sudoku/GameBoard.java
@@ -4,7 +4,7 @@ public class GameBoard
 {
     int[][] gameBoard;
 
-    // merhaba
+    // merhaba cbhd
     public GameBoard(int dimensionNumber)
     {
         this.gameBoard = new int[dimensionNumber][dimensionNumber];
